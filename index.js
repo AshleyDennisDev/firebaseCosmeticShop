113b7bed0f934daf97e8d3b9bc5769fa74d03c3c
Ashley N Dennis <a.n.d@Ashleys-MacBook-Air.local>
2022-01-28 16:53:30 -0500

    adding cosmetic db

---
+++ index.js
@@ -0,0 +1,25 @@
+const {initializeApp, cert } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const credentials = require('./credentials.json');
+
+initializeApp({
+    credential: cert(credentials)
+});
+
+const db = getFirestore();
+
+db.collection('Customers').doc('wZxl2vqcVg4ZEpDQlmNa').get()
+.then(doc => {
+    console.log(doc.id, ' => ', doc.data());
+})
+.catch(err => console.error(err));
+
+// db.collection('Products').where('Item','==','Lipstick').get()
+// .then(snapshot => {
+//     snapshot.forEach(doc => {
+//         console.log(doc.data());
+//     });
+// })
+// .catch(console.error);
+
+
