fedd49ff635cb299c8d42344b49b9570849c1a8e
Ashley N Dennis <a.n.d@Ashleys-MacBook-Air.local>
2022-01-28 17:02:43 -0500

    uncomment code

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ db.collection('Customers').doc('wZxl2vqcVg4ZEpDQlmNa').get()
 })
 .catch(err => console.error(err));
 
-// db.collection('Products').where('Item','==','Lipstick').get()
-// .then(snapshot => {
-//     snapshot.forEach(doc => {
-//         console.log(doc.data());
-//     });
-// })
-// .catch(console.error);
+db.collection('Products').where('Item','==','Lipstick').get()
+.then(snapshot => {
+    snapshot.forEach(doc => {
+        console.log(doc.data());
+    });
+})
+.catch(console.error);
 
 
